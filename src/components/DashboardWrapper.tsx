d79570b472553c948859678815edd38399f81b7c
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-09-28 15:37:04 -0300

    create new DashboardWrapper component

---
+++ src/components/DashboardWrapper.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/lib/utils"
+
+export const DashboardWrapper = ({
+    children
+}: {
+    children: React.ReactNode
+}) => {
+    return (
+        <div className="flex min-h-screen w-full bg-neutral-100 text-neutral-900">
+            <aside className="">
+                {/* Sidebar */}
+            </aside>
+            <main className={cn("flex w-full flex-col bg-neutral-100 dark:bg-neutral-900 md:pl-64")}>
+                {/* Navbar */}
+                {children}
+            </main>
+        </div>
+    )
+}
