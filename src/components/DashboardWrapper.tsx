2f463e9c1753e303983a4073eb8b8d83fe10dd41
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-09-28 20:15:53 -0300

    configure DashboardLayout to use the state from redux

--- src/components/DashboardWrapper.tsx
+++ src/components/DashboardWrapper.tsx
@@ -1,7 +1,11 @@
-import { cn } from "@/lib/utils"
-import { Navbar } from "./Navbar"
-import { Sidebar } from "./Sidebar"
-import StoreProvider from "@/redux/store/StoreProvider"
+"use client";
+
+import { cn } from "@/lib/utils";
+import { useEffect } from "react";
+import StoreProvider, { useAppSelector } from "@/redux/store/StoreProvider";
+
+import { Navbar } from "./Navbar";
+import { Sidebar } from "./Sidebar";
 
 
 export const DashboardLayout = ({
@@ -9,12 +13,25 @@ export const DashboardLayout = ({
 }: {
     children: React.ReactNode
 }) => {
+    const isSidebarCollapsed = useAppSelector(state => state.global.isSidebarCollapsed);
+    const isDarkMode = useAppSelector(state => state.global.isDarkMode);
+
+    useEffect(() => {
+        if (isDarkMode) {
+            document.documentElement.classList.add("dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+        }
+    }, [isDarkMode]);
+
     return (
         <div className="flex min-h-screen w-full bg-neutral-100 text-neutral-900">
             <aside className="">
                 <Sidebar />
             </aside>
-            <main className={cn("flex w-full flex-col bg-neutral-100 dark:bg-neutral-900 md:pl-64")}>
+            <main className={cn("flex w-full flex-col bg-neutral-100 dark:bg-neutral-900",
+                isSidebarCollapsed ? "md:pl-0" : "md:pl-64",
+            )}>
                 <Navbar />
                 {children}
             </main>
