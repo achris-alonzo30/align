26d5aa89039e8111eae590a28e170305766911d4
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-09-30 08:35:37 -0300

    create interface for Task model schema

--- client/src/redux/types.ts
+++ client/src/redux/types.ts
@@ -5,3 +5,29 @@ export interface Project {
     startDate?: string,
     endDate?: string
 }
+
+export interface Task {
+    id: number;
+    tags?: string;
+    title: string;
+    status?: Status;
+    points?: number;
+    dueDate?: string;
+    priority?: Priority;
+    projectId: number;
+    startDate?: string;
+    description?: string;
+    authorUserId?: number;
+    assignedUserId?: number;
+}
+
+export interface Status {
+
+}
+
+export enum Priority {
+    Low = "Low",
+    Medium = "Medium",
+    High = "High",
+    Urgent = "Urgent"
+}
