fcb12eecbbdd24436262d8ce80d154a3e8d03eb1
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-09-30 07:36:00 -0300

    add createTask in task POST route

--- server/src/routes/task.routes.ts
+++ server/src/routes/task.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { getTasks } from "../controllers/task.controller";
+import { createTask, getTasks } from "../controllers/task.controller";
 
 const router = express.Router();
 
 router.route("/")
     .get(getTasks)
+    .post(createTask)
 
 
 export default router;
