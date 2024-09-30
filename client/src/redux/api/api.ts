306fb4a7fa7057f77205078262fa074284134f4d
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-09-30 09:02:09 -0300

    create API endpoint to getProject in redux createApi

--- client/src/redux/api/api.ts
+++ client/src/redux/api/api.ts
@@ -1,13 +1,17 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { Project } from "../types";
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL
     }),
     reducerPath: "api",
-    tagTypes: [],
+    tagTypes: ["Projects"],
     endpoints: (builder) => ({
-
+        getProjects: builder.query<Project[], void>({
+            query: () => "/projects",
+            providesTags: ["Projects"],
+        })
     })
 });
 
