f533c6c27bf3e3140a2e0774a9a0102b63ff6aec
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-09-30 14:02:05 -0300

    create new TabButton component

--- client/src/app/projects/[projectId]/ProjectHeader.tsx
+++ client/src/app/projects/[projectId]/ProjectHeader.tsx
@@ -1,4 +1,5 @@
 import { PageHeader } from "@/components/PageHeader";
+import { cn } from "@/lib/utils";
 import { useState } from "react";
 
 
@@ -18,6 +19,33 @@ export const ProjectHeader = ({
                 <PageHeader name="Product Design Development" />
             </nav>
         </header>
+    )
+}
+
+interface TabButtonProps {
+    name: string;
+    activeTab: string;
+    icon: React.ReactNode;
+    setActiveTab: (activeTab: string) => void;
+}
+
+const TabButton = ({
+    name,
+    icon,
+    activeTab,
+    setActiveTab
+}: TabButtonProps) => {
+    const isActive = activeTab === name
+    return (
+        <button
+            className={cn("relative flex items-center gap-2 px-1 py-2 text-muted-foreground after:absolute after:-bottom-[9px] after:left-0 after-[1px] after:w-full hover:text-blue-600 dark:hover:text-neutral-100 sm:px-2 lg:px-4",
+                isActive ? "text-blue-600 after:bg-blue-600 dark:text-neutral-100" : "",
+            )}
+            onClick={() => setActiveTab(name)}
+        >
+            {icon}
+            {name}
+        </button>
     )
 }
 
